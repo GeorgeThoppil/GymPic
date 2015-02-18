bc7d38db82f482acebf5e8e0b7c5a9868310b08e
killzon567 <georgethoppil2@gmail.com>
2015-02-18 01:40:13 -0500

    Added Project

---
+++ server.js
@@ -0,0 +1,27 @@
+/**
+ * New node file
+ */
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8080);
